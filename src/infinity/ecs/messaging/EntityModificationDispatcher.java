60de7c2b74d8661354319c83827e6948a3bc6aa0
preip <p.reipschlaeger@googlemail.com>
2015-02-03 23:42:33 +0100

    Finished entity modification messaging system (for the moment)

--- src/infinity/ecs/messaging/EntityModificationDispatcher.java
+++ src/infinity/ecs/messaging/EntityModificationDispatcher.java
@@ -13,18 +13,18 @@ import java.util.Map;
  * Class that handles the dispatching of entity modification messages to all registered endpoints
  * that have registered under the ComponentType of the message. Also handles registering and
  * deregistering of endpoints and the creation of new endpoints.
- * <p>
- * <b>Dev-Notes:</b><br>
- * System may be inflexible, because messages consist only of the manipulated entity and the type
- * of component that has been manipulated. It should be evaluated if that is sufficient for all
- * cases or if there is a need for custom modification messages.
- * The advantage of the current version is the avoidance of the creation of new message objects
- * by simply passing references, thus saving memory and speed.
  * 
  * @author preip
- * @version 0.7
  */
 public final class EntityModificationDispatcher {
+	
+	 // Dev-Notes:
+	 // System may be inflexible, because messages consist only of the manipulated entity and the type
+	 // of component that has been manipulated. It should be evaluated if that is sufficient for all
+	 // cases or if there is a need for custom modification messages.
+	 // The advantage of the current version is the avoidance of the creation of new message objects
+	 // by simply passing references, thus saving memory and speed.
+	
 	/**
 	 * A list of all registered message endpoints indexed by the content types for which they
 	 * have registered. Used to dispatch messages to all relevant recipients.
@@ -53,14 +53,20 @@ public final class EntityModificationDispatcher {
 	/**
 	 * Dispatches a message consisting of the manipulated entity and the type of the component
 	 * which has been manipulated to all registered endpoints that the message concerns.
+	 * 
+	 * @param sender The endpoint which initiates the dispatch
+	 * @param entity The entity which has been modified
+	 * @param componentType The type of the component which has been modified
 	 */
-	public void dispatch(Entity entity, ComponentType componentType) {
+	public void dispatch(EntityModificationEndpoint sender, Entity entity,
+			ComponentType componentType) {
 		List<EntityModificationEndpoint> regEntry = _register.get(componentType);
 		// if there is an entry for the content type specified in the message, dispatch
-		// the message to every endpoint in the list
+		// the message to every endpoint in the list, apart from the sender
 		if (regEntry != null)
 			for (EntityModificationEndpoint endpoint : regEntry)
-				endpoint.receive(entity, componentType);
+				if (endpoint != sender)
+					endpoint.receive(entity, componentType);
 	}
 	
 	//----------------------------------------------------------------------------------------------
@@ -69,27 +75,36 @@ public final class EntityModificationDispatcher {
 
 	/**
 	 * Register the specified endpoint with the specified component types.
+	 * 
+	 * @param endpoint The endpoint which wants to register a set of component types
+	 * @param componentTypes A set of component for which the endpoint wants to register
 	 */
-	public void registerReceiver(EntityModificationEndpoint endpoint,
+	public void register(EntityModificationEndpoint endpoint,
 			ComponentType... componentTypes) {
 		for (ComponentType cType : componentTypes)
-			registerReceiver(endpoint, cType);
+			registerInternal(endpoint, cType);
 	}
 	
 	/**
 	 * Register the specified endpoint with the specified component types.
+	 * 
+	 * @param endpoint The endpoint which wants to register a set of component types
+	 * @param componentTypes A set of component for which the endpoint wants to register
 	 */
-	public void registerReceiver(EntityModificationEndpoint endpoint,
+	public void register(EntityModificationEndpoint endpoint,
 			List<ComponentType> componentTypes) {
 		Iterator<ComponentType> iter = componentTypes.iterator();
 		while(iter.hasNext())
-			deregisterReceiver(endpoint, iter.next());
+			registerInternal(endpoint, iter.next());
 	}
 	
 	/**
 	 * Internal method which handles the registration of a endpoint to a single component type.
+	 * 
+	 * @param endpoint The endpoint which should be registered
+	 * @param componentType The type of component the endpoint wants to register to
 	 */
-	private void registerReceiver(EntityModificationEndpoint endpoint,
+	private void registerInternal(EntityModificationEndpoint endpoint,
 			ComponentType componentType) {
 		List<EntityModificationEndpoint> regEntry = _register.get(componentType);
 		// if there already is a register entry for the current component type, simply add the
@@ -110,20 +125,38 @@ public final class EntityModificationDispatcher {
 	// Deregistering
 	//----------------------------------------------------------------------------------------------
 	
-	public void deregisterReceiver(EntityModificationEndpoint endpoint,
+	/**
+	 * Deregister the specified endpoint with the specified component types.
+	 * 
+	 * @param endpoint The endpoint which should be deregistered
+	 * @param componentTypes The type of component the endpoint wants to deregister from
+	 */
+	public void deregister(EntityModificationEndpoint endpoint,
 			ComponentType... componentTypes) {
 		for (ComponentType cType : componentTypes)
-			registerReceiver(endpoint, cType);
+			deregisterInternal(endpoint, cType);
 	}
 	
-	public void deregisterReceiver(EntityModificationEndpoint endpoint,
+	/**
+	 * Deregister the specified endpoint with the specified component types.
+	 * 
+	 * @param endpoint The endpoint which should be deregistered
+	 * @param componentTypes The type of component the endpoint wants to deregister from
+	 */
+	public void deregister(EntityModificationEndpoint endpoint,
 			List<ComponentType> componentTypes) {
 		Iterator<ComponentType> iter = componentTypes.iterator();
 		while(iter.hasNext())
-			deregisterReceiver(endpoint, iter.next());
+			deregisterInternal(endpoint, iter.next());
 	}
 	
-	private void deregisterReceiver(EntityModificationEndpoint endpoint,
+	/**
+	 * Internal method which handles the deregistration of a endpoint from a single component type.
+	 * 
+	 * @param endpoint The endpoint which should be deregistered
+	 * @param componentType The type of component the endpoint wants to register from
+	 */
+	private void deregisterInternal(EntityModificationEndpoint endpoint,
 			ComponentType componentType) {
 		List<EntityModificationEndpoint> regEntry = _register.get(componentType);
 		if (regEntry != null)
